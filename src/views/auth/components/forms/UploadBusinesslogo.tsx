8e81c59e1a767a07a9082620511ded7416450510
Ozichukwu Ezike <ozichukwuezike@Ozichukwus-MBP.ipnxng.net>
2021-05-26 14:57:07 +0100

    refactor: made logo upload on addBusinessForm a component

--- src/views/auth/components/forms/UploadBusinesslogo.tsx
+++ src/views/auth/components/forms/UploadBusinesslogo.tsx
@@ -5,7 +5,6 @@ import { UploadBusinessLogoWrapper } from './styles'
 
 
 const upload = 'https://image.shutterstock.com/image-vector/upload-icon-trendy-flat-style-260nw-1697806396.jpg'
-const imageStyle = {  }
 
 const UploadPicture = () => {
   return (

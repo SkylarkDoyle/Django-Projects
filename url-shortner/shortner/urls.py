aac618d200c4921a914811633dad382a6806e691
SkylarkDoyle <78934202+SkylarkDoyle@users.noreply.github.com>
2021-08-08 06:04:19 -0700

    Delete url-shortner directory

--- url-shortner/shortner/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, create, go
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('create', create, name='create'),
-    path('<str:pk>', go, name='go')
-]
